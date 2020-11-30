5cb087dbab172d0c0d8a1772395c3178c4ad73a3
ISC_025 <ISC_025@ISC_025.homenet.telecomitalia.it>
2020-11-30 09:44:16 +0100

    Creati da Fabio

---
+++ bootjobs-hibernate-maven/src/main/java/it/bootjobs/model/Candidato_service.java
@@ -0,0 +1,19 @@
+package it.bootjobs.model;
+
+public class Candidato_service 
+{
+	  public boolean iscrizione(Candidato c_isc)
+	  {
+		  boolean check = false;
+		  return check;
+	  }
+	  public Candidato modificaProfilo(Candidato c_mod)
+	  {
+		  return c_mod;
+	  }
+	  public boolean eliminaProfilo(int id)
+	  {
+		  boolean check = false;
+		  return check;
+	  }
+}
---
+++ bootjobs-hibernate-maven/src/main/java/it/bootjobs/model/Candidatura_service.java
@@ -0,0 +1,19 @@
+package it.bootjobs.model;
+
+public class Candidatura_service 
+{
+	   public boolean inserisciCandidatura(Candidatura ins)
+	   {
+		boolean check = false;
+		return check;
+	   }
+	   public Candidatura modificaCandidatura(Candidatura candidancy)
+	   {
+		   return candidancy;
+	   }
+	   public boolean eliminaCandidatura(int id)
+	   {
+		   boolean check = false;
+			return check;
+	   }
+}
